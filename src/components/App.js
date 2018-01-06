9bc1258f3cc1475b9d2691fe70bd0770fb1093c9
JohnDeegs <john.deegan@hotmail.com>
2018-01-06 18:42:43 +0000

    prep for sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Nav from './Nav';
 import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <span>Hello World</span>
+        <Nav></Nav>
       </div>
     );
   }
