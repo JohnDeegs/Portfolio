c5f8bc0505db1d52f1e8848eb5660480726494e8
JohnDeegs <john.deegan@hotmail.com>
2018-01-07 02:03:18 +0000

    custom responsive nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
-import '../css/App.css';
+import '../css/App.scss';
 
 class App extends Component {
   render() {
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
+import '../css/Nav.scss';
 
 class Nav extends Component {
     render(){
         return(
-        <div>
-            
+        <div className="nav">
+            <div className="right-logo">
+                <span>
+                    John Deegan
+                </span>
+            </div>
+            <ul>
+                <li>
+                    <a href="#">Contact</a>
+                    <i className="down-arrow"></i>
+                </li>
+                <li>
+                    <a href="#">About Me</a>
+                    <i className="down-arrow"></i>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                    <i className="down-arrow"></i>
+                </li>
+            </ul>
         </div>
         )
     }
