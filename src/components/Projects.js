21aa06f13e0a2f9725e64158a1e56782fcaaa4a1
JohnDeegs <john.deegan@hotmail.com>
2018-01-21 20:54:28 +0000

    21012018

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import'../css/Projects.scss';
 import colOne from '../css/img/col-one.jpg';
 import colTwo from '../css/img/col-two.jpg';
 import colThree from '../css/img/col-three.jpg';
+import colFour from '../css/img/col-four.jpg';
 
 
 class Projects extends Component{
@@ -16,20 +17,32 @@ class Projects extends Component{
                 <div className="row">
                     <div className="col-md-4 col-sm-12 no-padding">
                         <div className="project-container-full project-container-one">
-                            
+                            <div className="box-content">
+                                <h2>React Project</h2>
+                                <a href="" className="button button-ghost ripple">View</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6 no-padding">
+                        <div className="project-container-half project-container-two">
+                            <div className="box-content">
+                                <h2>React Project</h2>
+                                <a href="" className="button button-ghost">View</a>
+                            </div>
+                        </div>
+                        <div className="project-container-half project-container-three">
+                            <div className="box-content">
+                                <h2>React Project</h2>
+                                <a href="" className="button button-ghost">View</a>
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-4 col-sm-12 no-padding">
-                        <div className="project-container-half project-container-two col-sm-6">
-                            
-                        </div>
-                        <div className="project-container-half project-container-two col-sm-6">
-                            
-                        </div>
-                    </div>
-                    <div className="col-md-4 col-sm-12 no-padding">
-                        <div className="project-container-full project-container-three">
-                            
+                        <div className="project-container-full project-container-four">
+                            <div className="box-content">
+                                <h2>React Project</h2>
+                                <a href="" className="button button-ghost">View</a>
+                            </div>
                         </div>
                     </div>
                     
